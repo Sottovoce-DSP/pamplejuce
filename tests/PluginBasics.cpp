d338c6efcc5ff1faebdde5b3440524660eb5168d
= <satbir.kira@gmail.com>
2024-11-09 20:56:33 -0500

    Master #comment adding version test

--- tests/PluginBasics.cpp
+++ tests/PluginBasics.cpp
@@ -2,6 +2,8 @@
 #include <PluginProcessor.h>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
+#include <fstream>
+#include <string>
 
 TEST_CASE ("one is equal to one", "[dummy]")
 {
@@ -19,7 +21,6 @@ TEST_CASE ("Plugin instance", "[instance]")
     }
 }
 
-
 #ifdef PAMPLEJUCE_IPP
     #include <ipp.h>
 
@@ -28,3 +29,20 @@ TEST_CASE ("IPP version", "[ipp]")
     CHECK_THAT (ippsGetLibVersion()->Version, Catch::Matchers::Equals ("2021.11.0 (r0xcd107b02)"));
 }
 #endif
+
+TEST_CASE ("Plugin version matches VERSION file", "[version]")
+{
+    // Read the version from the VERSION file
+    std::ifstream versionFile("VERSION");
+    REQUIRE(versionFile.is_open()); // Ensure the file is accessible
+
+    std::string fileVersion;
+    std::getline(versionFile, fileVersion);
+    versionFile.close();
+
+    // Create a PluginProcessor instance
+    PluginProcessor testPlugin;
+
+    // Check that the version in the file matches the version reported by the plugin
+    CHECK(fileVersion == testPlugin.getVersion().toStdString());
+}
